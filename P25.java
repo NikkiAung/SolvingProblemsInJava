2a260b3a82cbbbc2bc934590d44fe919a4da5130
Aung Nanda Oo <160472666+NikkiAung@users.noreply.github.com>
2024-03-02 20:56:09 -0800

    P25.java

--- P25.java
+++ P25.java
@@ -1,6 +1,7 @@
-//RemovingZeroToRight
+//Both of the solution are leading to move the zero including in the array to the right
 import java.util.Arrays;
 
+//#1
 class P25{
 
     public static int[] removingZeroToRight(int[] nums){
@@ -26,5 +27,27 @@ class P25{
     public static void main(String args[]){
         int[] nums = {0,1,0,4,12,0};
         System.out.println(Arrays.toString(removingZeroToRight(nums)));
+    }
+}
+
+//#2
+class Tester1{
+
+    public static int[] movingZero(int[] nums, int n){
+        int j = 0;
+        for(int i = 0; i < n; i++){
+            if(nums[i] != 0 && nums[j] == 0){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }if(nums[j] != 0){
+                j++;
+            }
+        }return nums;
+    }
+    public static void main(String args[]){
+        int[] nums = {8,1,0,0,2,1,};
+        int length = nums.length;
+        System.out.println(Arrays.toString(movingZero(nums, length)));
     }
 }
